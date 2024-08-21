2d72f2da5e457fc29fddd6b5e468f9a9e0cb6b24
Julia <s2107370@ed.ac.uk>
2024-08-21 20:01:24 +0100

    pressure sensor initial measurements

---
+++ pressure-sensor-DPS310/test-dps310.py
@@ -0,0 +1,17 @@
+import time
+import board
+from adafruit_dps310.basic import DPS310
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+dps310 = DPS310(i2c)
+
+dps310.sea_level_pressure = 1007
+
+while True:
+    print("Temperature = %.2f *C" % dps310.temperature)
+    print("Pressure = %.2f hPa" % dps310.pressure)
+    print("Sea Level Pressure = %.2f hPa" % dps310.sea_level_pressure)
+    print("Altitude = %.2f m above sea level" % dps310.altitude)
+    print("")
+    time.sleep(1.0)
